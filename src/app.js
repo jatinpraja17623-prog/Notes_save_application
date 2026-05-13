373367297d30b0a3ed0ebccb23db84f85f9a558e
jatinp17623 <jatinp17623@gmail.com>
2026-05-13 16:19:04 +0530

    update cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,12 @@ const notesRoutes = require('../src/routes/notes.routes');
 
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://notes-save-application.vercel.app"
+  ]
+}));
 app.use(express.json());
 app.use(cookieParser());
 
