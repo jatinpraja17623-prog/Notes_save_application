258c996f2045e36f93a1447f87a0bc6a5ade802f
jatinp17623 <jatinp17623@gmail.com>
2026-05-13 17:40:12 +0530

    remove node_modules

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@
 //  HOW IT WORKS: Every function calls your backend and returns data
 // ============================================================
 
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "notessaveapplication-production.up.railway.app";
 
 function authHeader() {
   const token = localStorage.getItem("token");
